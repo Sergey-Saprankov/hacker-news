15d442770e81269d82ad8cff5543869593fcddfd
Sergey Saprankov <sergey-saprankov@mail.ru>
2023-04-28 14:39:24 +0300

    add path

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -3,13 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import { News } from 'pages/News'
 import { NewsList } from 'pages/NewsList'
 import { PageNotFound } from 'pages/PageNotFound'
+import { Path } from 'shared/constants/path/Path'
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path={'/'} element={<NewsList />} />
-      <Route path={'news/:id'} element={<News />} />
-      <Route path={'*'} element={<PageNotFound />} />
+      <Route path={Path.HOME} element={<NewsList />} />
+      <Route path={Path.NEWS} element={<News />} />
+      <Route path={Path.PAGE_NOT_FOUND} element={<PageNotFound />} />
     </Routes>
   )
 }
---
+++ src/shared/constants/path/Path.ts
@@ -0,0 +1,5 @@
+export enum Path {
+  HOME = '/',
+  NEWS = 'news/:id',
+  PAGE_NOT_FOUND = '*',
+}
